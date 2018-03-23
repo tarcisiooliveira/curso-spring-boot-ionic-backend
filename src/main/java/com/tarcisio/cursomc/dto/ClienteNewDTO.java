951275e6c2fd2fb5e5d01dd64799089dc7d9f49b
Tarcisio Oliveira <tarcisio@ymail.com>
2018-03-22 23:14:32 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/tarcisio/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/tarcisio/cursomc/dto/ClienteNewDTO.java
@@ -2,28 +2,46 @@ package com.tarcisio.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.tarcisio.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Length(min = 5, max = 120, message = "O tamanho tem que ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Email(message = "Email invalido")
 	private String email;
+
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String logradouro;
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
-	
+
 	private Integer cidadeId;
-	
+
 	public ClienteNewDTO() {
-		
+
 	}
 
 	public String getNome() {
@@ -130,7 +148,4 @@ public class ClienteNewDTO implements Serializable {
 		this.cidadeId = cidadeId;
 	}
 
-	
-	
-	
 }
