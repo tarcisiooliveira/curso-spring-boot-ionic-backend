ffe77bb7293bd3eb19a754edf5d1edae4ffb695a
Tarcisio Oliveira <tarcisio@ymail.com>
2018-03-25 19:42:15 -0300

    Validação customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/tarcisio/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/tarcisio/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.tarcisio.cursomc.domain.Cliente;
+import com.tarcisio.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
